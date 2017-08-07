5cd29b9f6b6397a6e9e2922456cefe54d84253e0
haialaluf <hai.alaluf@yahoo.com>
2017-08-07 18:28:38 +0300

    Update replaceColor.js

--- replaceColor.js
+++ replaceColor.js
@@ -4,17 +4,17 @@ replaceColor = (oldColor, newColor) => {
         return result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
     }
 	var replace = (el) => {
-	if ($(el).children().length)
-		$(el).children().each((i, e)=>replace(e))
-	else {
-		if ($(el).css("background-color") === hexToRgb(oldColor)) 
-			$(el).css("background-color", hexToRgb(newColor))
-		if ($(el).css("border-color") === hexToRgb(oldColor)) 
-			$(el).css("border-color", hexToRgb(newColor))
-		if ($(el).css("color") === hexToRgb(oldColor)) 
-			$(el).css("color", hexToRgb(newColor))
+		if (el.style.backgroundColor === hexToRgb(oldColor)) 
+			el.style.backgroundColor = hexToRgb(newColor);
+		if (el.style.borderColor === hexToRgb(oldColor)) 
+			el.style.borderColor = hexToRgb(newColor);
+		if (el.style.color === hexToRgb(oldColor)) 
+			el.style.color = hexToRgb(newColor);
 
+	}
+	var all = document.getElementsByTagName("*");
+
+    for (var i=0, max=all.length; i < max; i++) {
+         replace(all[i])
     }
-}
-	replace('body')
 }
